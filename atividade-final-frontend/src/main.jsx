40f5aa0f0c181f0e594804d3864461c3f87acff7
liviacnasc <liviac.nasc@gmail.com>
2025-02-23 15:40:11 -0300

    agora vai 3?

--- atividade-final-frontend/src/main.jsx
+++ atividade-final-frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { config } from 'dotenv';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
